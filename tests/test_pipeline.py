07374a583241fb60f52c81b613c6bffc99f9a441
Mamello Molokwane <161704049+MamelloMolokwane@users.noreply.github.com>
2026-06-24 14:08:06 +0000

    test: write tests for your pipeline.

---
+++ tests/test_pipeline.py
@@ -0,0 +1,20 @@
+from scripts.pipeline import extract, transform, load
+import unittest
+
+# Don't forget to write tests
+
+class TestPipeline:
+
+    def test_extract():
+        ...
+    
+    def test_bronze_layer_exists():
+        ...
+
+    def test_silver_layer_exists():
+        ...
+
+    def test_tranform():
+        ...
+
+    
