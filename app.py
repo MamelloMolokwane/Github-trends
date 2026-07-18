2ad21c4002f13c59af365d8803ecf878f2d45268
Mamello Molokwane <161704049+IndigoBlack@users.noreply.github.com>
2026-07-18 13:42:21 +0200

    feat: Added spinner when creating pipleine and created a few more charts

--- app.py
+++ app.py
@@ -12,77 +12,123 @@ gold_layer = "./data/gold/github-trends.db"
 os.makedirs(os.path.dirname(bronze_layer), exist_ok=True)
 os.makedirs(os.path.dirname(silver_layer), exist_ok=True)
 os.makedirs(os.path.dirname(gold_layer), exist_ok=True)
+LAST_RUN_FILE = "last_run.txt"
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 @app.route("/etl/run")
-def execute_etl_pipeline(): # Change this into a function that just executes the pipeline.
+def execute_etl_pipeline():
+    today = datetime.date.today().isoformat()
+    if os.path.exists(LAST_RUN_FILE):
+        with open(LAST_RUN_FILE) as file:
+            if file.read().strip() == today:
+                return jsonify({
+                    "status": "blocked",
+                    "message": "Pipeline already ran today."
+                }), 403
+            
     etl.extract(bronze_layer)
     etl.transform(bronze_layer, silver_layer)
     etl.load_dimensions(silver_layer, gold_layer)
     etl.load_facts(silver_layer, gold_layer)
 
-    etl.table_check(gold_layer)
+    return jsonify({
+        "status": "success",
+        "message": "Pipeline completed."
+    })
 
+def query(sql):
     conn = sqlite3.connect(gold_layer)
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM fact_repo_snapshot")
-    fact_data = [dict(row) for row in cursor.fetchall()]
-    
-    return jsonify(fact_data)
+    cursor.execute(sql)
+    rows = [dict(r) for r in cursor.fetchall()]
+
+    conn.close()
+    return rows
 
 @app.route("/languages")
 def languages_data():
-    conn = sqlite3.connect(gold_layer)
-    conn.row_factory = sqlite3.Row
-    cursor = conn.cursor()
-
-    cursor.execute("""
+    sql = """
     SELECT
         l.language_name,
         SUM(f.stars) AS total_stars
     FROM fact_repo_snapshot f
     JOIN dim_language l
-    ON f.language_key=l.language_key
+        ON f.language_key=l.language_key
     GROUP BY l.language_name
-    ORDER BY total_stars DESC;
-    """)
-
-    languages_data = [dict(row) for row in cursor.fetchall()]
-
-    conn.close()
+    ORDER BY total_stars DESC
+    LIMIT 10;
+    """
     
-    return jsonify(languages_data)
+    return jsonify(query(sql))
 
 @app.route("/repos")
 def load_repos():
-    conn = sqlite3.connect(gold_layer)
-    conn.row_factory = sqlite3.Row
-    cursor = conn.cursor()
-
-    cursor.execute("""
+    sql = """
     SELECT
         l.repo_name,
         SUM(f.stars) AS total_stars
     FROM fact_repo_snapshot f
     JOIN dim_repository l
-    ON f.repo_key=l.repo_key
+        ON f.repo_key=l.repo_key
     GROUP BY l.repo_name
     ORDER BY total_stars DESC
     LIMIT 10;
-    """)
-
-    repo_data = [dict(row) for row in cursor.fetchall()]
-    conn.close()
+    """
     
-    return jsonify(repo_data)
+    return jsonify(query(sql))
 
+@app.route("/owners")
+def load_owners():
+    sql = """
+    SELECT
+        o.owner_name,
+        SUM(f.stars) AS total_stars
+    FROM fact_repo_snapshot f
+    JOIN dim_owner o
+        ON f.owner_key = o.owner_key
+    GROUP BY o.owner_name
+    ORDER BY total_stars DESC
+    LIMIT 10;
+    """
+                   
+    return jsonify(query(sql))
 
+@app.route("/forked_repositories")
+def most_forked_repositories():
+    sql = """
+    SELECT 
+    r.repo_name,
+    SUM(f.forks) AS total_forks
+    FROM fact_repo_snapshot f
+    JOIN dim_repository r
+        ON r.repo_key = f.repo_key
+    GROUP BY r.repo_name
+    ORDER BY total_forks DESC
+    LIMIT 10;
+    """
+                   
+    return jsonify(query(sql))
 
+@app.route("/watchers")
+def most_watcher():
+    sql = """
+    SELECT 
+        r.repo_name,
+        SUM(f.watchers) AS total_watchers
+    FROM fact_repo_snapshot f
+    JOIN dim_repository r
+        ON f.repo_key = r.repo_key
+    GROUP BY r.repo_name
+    ORDER BY total_watchers DESC
+    LIMIT 10;
+    """
+                   
+    return jsonify(query(sql))
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,14 +1,39 @@
 // Create a button to run the pipeline
 
-document.getElementById("run-pipeline").addEventListener("click", async function(event) {
+let languageCanvasChart;
+let repoCanvasChart;
+let ownerCanvasChart;
+let forkCanvasChart;
+let watchersCanvasChart;
+
+const loader = document.getElementById("loader");
+const pipelineButton = document.getElementById("run-pipeline");
+pipelineButton.addEventListener("click", async function(event) {
+    
     try {
+        pipelineButton.disabled = true;
+        loader.classList.remove("hidden");
         const response = await fetch("/etl/run");
         const data = await response.json();
+        if (!response.ok) {
+            alert(result.message);
+            return;
+        }
         console.log("Pipeline Started...")
-        await loadLanguages();
-        await loadRepos();
+        await Promise.all([
+            loadLanguages(),
+            loadRepos(),
+            loadOwners(),
+            loadForks(),
+            loadWatchers()
+        ]);
+        localStorage.setItem("lastPipelineRun", today);
+        pipelineButton.textContent = "Already ran today";
     } catch(error) {
         console.log("Pipeline failed to start: ", error)
+        pipelineButton.disabled = false;
+    } finally {
+        loader.classList.add("hidden");
     }
 });
 
@@ -17,19 +42,41 @@ async function loadLanguages() {
     const data = await response.json();
     const languages = data.map(project => project.language_name)
     const starCount = data.map(project => project.total_stars);
-
     const languageChart = document.getElementById("languages-bar-chart");
-    new Chart(languageChart, {
+
+    if (languageCanvasChart) {
+        languageCanvasChart.destroy();
+    }
+    
+    languageCanvasChart = new Chart(languageChart, {
         type: "bar",
         data: {
             labels: languages, // Languages go in here
             datasets: [
                 {
-                    label: "Languages",
+                    label: "Stars",
                     data: starCount
                 }
             ]
-        }
+        },
+        options:{
+            plugins: {
+                title: {
+                    display: true,
+                    text: "Top Languages by Stars"
+                }},
+            scales:{
+                x:{
+                    title:{
+                        display:true,
+                        text:"Language"
+                    }},
+                y:{
+                    title:{
+                        display:true,
+                        text: "Stars"
+                    }}
+            }}
     })
 };
 
@@ -40,19 +87,184 @@ async function loadRepos() {
     const starsCount = data.map(project => project.total_stars);
     const repoChart = document.getElementById("repos-bar-chart");
 
-    new Chart(repoChart, {
+    if (repoCanvasChart) {
+        repoCanvasChart.destroy();
+    }
+
+    repoCanvasChart = new Chart(repoChart, {
         type: "bar",
         data: {
             labels: repos,
             datasets: [{
-                label: "Top 10 repositories",
+                label: "Stars",
                 data: starsCount,
             }]
-        }
+        },
+        options:{
+            plugins: {
+                title: {
+                    display: true,
+                    text: "Top 10 repositories"
+                }},
+            scales:{
+                x:{
+                    ticks: {
+                        maxRotation: 45,
+                        minRotation: 45
+                    },
+                    title:{
+                        display:true,
+                        text: "Repository"
+                    }},
+                y:{
+                    title:{
+                        display:true,
+                        text: "Stars"
+                    }}
+            }}
+    })
+};
+
+async function loadForks() {
+    const response = await fetch("/forked_repositories");
+    const data = await response.json();
+    const repos = data.map(project => project.repo_name);
+    const forkCount = data.map(project => project.total_forks);
+    const forkChart = document.getElementById("forks-bar-chart");
+
+    if (forkCanvasChart) {
+        forkCanvasChart.destroy();
+    }
+
+    forkCanvasChart = new Chart(forkChart, {
+        type: "bar",
+        data: {
+            labels: repos,
+            datasets: [{
+                label:  "Forks",
+                data: forkCount,
+            }]
+        },
+        options:{
+            plugins: {
+                title: {
+                    display: true,
+                    text: "Top 10 most forked repositories"
+                }},
+            scales:{
+                x:{
+                    ticks: {
+                        maxRotation: 45,
+                        minRotation: 45,
+                        padding: -3
+                    },
+                    title:{
+                        display:true,
+                        text: "Repository"
+                    }},
+                y:{
+                    title:{
+                        display:true,
+                        text: "Forks"
+                    }}
+            }}
+    })
+};
+
+async function loadOwners() {
+    const response = await fetch("/owners");
+    const data = await response.json();
+    const owners = data.map(repo => repo.owner_name);
+    const starCount = data.map(repo => repo.total_stars);
+    const ownerChart = document.getElementById("owners-bar-chart");
+
+    if (ownerCanvasChart) {
+        ownerCanvasChart.destroy();
+    }
+
+    ownerCanvasChart = new Chart(ownerChart, {
+        type: "bar",
+        data: {
+            labels: owners,
+            datasets: [{
+                label: "Stars",
+                data: starCount,
+            }]
+        },
+        options:{
+            plugins: {
+                title: {
+                    display: true,
+                    text: "Top 10 owners with the most starred repositories"
+                }},
+            scales:{
+                x:{
+                    ticks: {
+                        maxRotation: 45,
+                        minRotation: 45
+                    },
+                    title:{
+                        display:true,
+                        text: "Owner"
+                    }},
+                y:{
+                    title:{
+                        display:true,
+                        text: "Stars"
+                    }}
+            }}
+    })
+}
+
+async function loadWatchers() {
+    const response = await fetch("/watchers");
+    const data = await response.json();
+    const repos = data.map(repo => repo.repo_name);
+    const watchersCount = data.map(repo => repo.total_watchers);
+    const watchersChart = document.getElementById("watchers-bar-chart");
+
+    if (watchersCanvasChart) {
+        watchersCanvasChart.destroy();
+    }
+
+    watchersCanvasChart = new Chart(watchersChart, {
+        type: "bar",
+        data: {
+            labels: repos,
+            datasets: [{
+                label: "Watchers",
+                data: watchersCount,
+            }]
+        },
+        options:{
+            plugins: {
+                title: {
+                    display: true,
+                    text: "Top 10 repositories"
+                }},
+            scales:{
+                x:{
+                    ticks: {
+                        maxRotation: 45,
+                        minRotation: 45
+                    },
+                    title:{
+                        display:true,
+                        text: "Repository"
+                    }},
+                y:{
+                    title:{
+                        display:true,
+                        text: "Watchers"
+                    }}
+            }}
     })
 };
 
 loadLanguages();
 loadRepos();
+loadOwners();
+loadForks();
+loadWatchers();
 
-// Add more charts
+// Add more charts - Mabe
